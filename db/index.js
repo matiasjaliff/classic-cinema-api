8eeded15a51b4ab4dfa6436b35f4743dd24f3a41
Matias Jaliff <matiasjaliff@gmail.com>
2022-11-29 18:16:58 -0300

    add enviroment DB

--- db/index.js
+++ db/index.js
@@ -4,14 +4,21 @@ const Sequelize = require("sequelize");
 
 // Load enviroment variables
 
-const {
-  dbName,
-  dbUser,
-  dbPass,
-  dbHost,
-  dbPort,
-  dbDialect,
-} = require("../config");
+// const {
+//   dbName,
+//   dbUser,
+//   dbPass,
+//   dbHost,
+//   dbPort,
+//   dbDialect,
+// } = require("../config");
+
+const dbName = process.env.DB_NAME;
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASSWORD;
+const dbHost = process.env.DB_HOST;
+const dbPort = process.env.DB_PORT;
+const dbDialect = process.env.DB_DIALECT;
 
 // Sequelize instance to connect to database
 
